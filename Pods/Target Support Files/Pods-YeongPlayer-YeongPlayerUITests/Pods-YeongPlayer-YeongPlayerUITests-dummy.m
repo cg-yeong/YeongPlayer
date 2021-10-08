5fa6cd3f0a861b9d505625730909630e07dd78b6
geunyeong <ds1506027@gmail.com>
2021-10-08 09:16:55 +0900

    delete pods/

--- Pods/Target Support Files/Pods-YeongPlayer-YeongPlayerUITests/Pods-YeongPlayer-YeongPlayerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YeongPlayer_YeongPlayerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_YeongPlayer_YeongPlayerUITests
-@end
--- Pods/Target Support Files/Pods-YeongPlayerTests/Pods-YeongPlayerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YeongPlayerTests : NSObject
-@end
-@implementation PodsDummy_Pods_YeongPlayerTests
-@end
